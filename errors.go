7f9b60b6718b2e70c31a71ce585eaf62df398bfb
Mya Pitzeruse <mya@effx.com>
2021-06-02 13:21:58 -0500

    setup golang ci-lint

--- errors.go
+++ errors.go
@@ -6,5 +6,5 @@ import (
 
 var (
 	ErrInitializeAfterStartup = fmt.Errorf("cannot initialize application after startup")
-	ErrRunOrStart             = fmt.Errorf("invalid state: cannot Start and Run an application in the same execution context")
+	ErrRunOrStart             = fmt.Errorf("cannot start and run an application in the same execution context")
 )
